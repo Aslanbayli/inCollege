5d9652481081c0dacea6a045b1daaf0fd3d344f3
Ali_Asl <48028559+Aslanbayli@users.noreply.github.com>
2023-06-10 23:35:27 -0400

    connect-option

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-import sys
 import time
 
 from user_auth import authentication as auth
 from user_auth import validator as valid
 from selection import selection_menu as select
+from util import util as util
 
 def main():
     users = {} # {username: [password, first_name, last_name]}
 
-    states = {"start_menu": 0, "promotional_video": 1, "logging_in": 2, "register": 3, "logged_in": 4} # Add more states as needed
+    states = {"start_menu": 0, "promotional_video": 1, "logging_in": 2, "register": 3, "logged_in": 4, "connect": 5} # Add more states as needed
     state = states["start_menu"] # Current state
 
-    auth.file_read(users) # Load the data from file into the dictionary for faster access
+    util.file_read(users) # Load the data from file into the dictionary for faster access
 
     while state in states.values():
-        #User Prompt Messages
-        if(state == states["start_menu"]):
-            user_auth = input("\n(w)atch promotional video | (l)ogin | (r)egister | (e)xit: ")
+        # User Prompt Messages
+        if (state == states["start_menu"]):
+            user_auth = input("\n(w)atch promotional video | (f)ind people you might know | (l)ogin | (r)egister | (e)xit: ")
             match user_auth.lower():
                 case "w":
                     state = states["promotional_video"]
+                case "f":
+                    state = states["connect"]
                 case "l":
                     state = states["logging_in"]
                 case "r":
@@ -30,14 +32,39 @@ def main():
                 case _:
                     print("\nInvalid option. Please try again.")
 
-        #Promotional Video
-        elif(state == states["promotional_video"]):
+        # Promotional Video
+        elif (state == states["promotional_video"]):
             print("\nVideo is now playing.")
             input("\nPress Enter when ready to return to the log in screen...")
             state = states["start_menu"]
 
-        #User Log-in
-        elif(state == states["logging_in"]):
+        # Connect
+        elif (state == states["connect"]):
+            print("\nFill out the prompts below to find people you might know.")
+            f_name = input("First Name: ")
+            l_name = input("Last Name: ")
+            found = util.connect(users, f_name, l_name)
+            if found:
+                print("\nThey are a part of the InCollege system.")
+
+                user_auth = input("\nJoin inCollege to connect with them. (l)ogin | (r)egister: ")
+                while not valid.validate_input_lrm(user_auth):
+                    print("\nInvalid option. Please try again.")
+                    user_auth = input("\nJoin inCollege to connect with them. (l)ogin | (r)egister: ")
+
+                if user_auth.lower() == "l":
+                    state = states["logging_in"]
+                elif user_auth.lower() == "r":
+                    state = states["register"]
+                else:
+                    state = states["start_menu"]
+            else:
+                print("\nThey are not yet a part of the InCollege system yet.")
+                state = states["start_menu"]
+                    
+
+        # User Log-in
+        elif (state == states["logging_in"]):
             print("\nEnter your credentiasls to login.")
             username = input("Username: ")
             password = input("Password: ")
@@ -46,7 +73,7 @@ def main():
             while not is_valid_login:
                 print("Incorrect username / password")
                 try_again = input("\nWould you like to try again? (y)es | (n)o: ")
-                while not valid.validate_input(try_again):
+                while not valid.validate_input_yn(try_again):
                     print("\nInvalid option. Please try again.")
                     try_again = input("\nWould you like to try again? (y)es | (n)o: ")
 
@@ -66,9 +93,9 @@ def main():
                 print("\nYou have succesfully logged in.")
                 select.selection_menu_options()
 
-        #User Register
-        elif(state == states["register"]):
-            if (auth.database_check(users) == True):
+        # User Register
+        elif (state == states["register"]):
+            if (util.database_check(users) == True):
                 print("All permitted accounts have been created, please come back later")
                 state = states["start_menu"]
                 continue
@@ -82,7 +109,7 @@ def main():
 
                 # Back "Button"
                 continue_register = input("\nContinue? (y/n):")
-                while not valid.validate_input(continue_register):
+                while not valid.validate_input_yn(continue_register):
                     print("\nInvalid option. Please try again.")
                     continue_register = input("\nContinue? (y/n):")
                     
@@ -107,7 +134,7 @@ def main():
 
                 # Back "Button"
                 continue_register = input("\nContinue? (y/n):")
-                while not valid.validate_input(continue_register):
+                while not valid.validate_input_yn(continue_register):
                     print("\nInvalid option. Please try again.")
                     continue_register = input("\nContinue? (y/n):")
 
@@ -125,7 +152,7 @@ def main():
             last_name = input("Last Name: ")
 
             auth.register(users, username, password, first_name, last_name)
-            auth.file_save(users)
+            util.file_save(users)
             state = states["logged_in"] # set the state to logged_in
             print("\nYou have succesfully created an account.")
             select.selection_menu_options()
@@ -135,6 +162,10 @@ def main():
             state = states["start_menu"]
 
 if __name__ == "__main__":
+    # Display the banner
+    with open("assets/banner.txt", "r") as file:
+        print(file.read())
+
     print("\n***** Welcome to inCollege app! *****")
     print("\n\nListen to an inspiring success story:")
     success_story = "\"I had problems gaining traction on LinkedIn. Through InCollege, I now have an internship at Raytheon!\""
--- selection/selection_menu.py
+++ selection/selection_menu.py
@@ -1,5 +1,5 @@
 from user_auth import validator as valid
-from user_auth import authentication as auth
+from util import util as util
 
 def selection_menu_options():
     while True:
@@ -13,7 +13,7 @@ def selection_menu_options():
             if choice == '1':
                 job_search()
             elif choice == '2':
-                print("\nUnder construction, check back later.")
+                print("\nUnder construction, check back later....")
             elif choice == '3':
                 skill_selection()
             elif choice == '4':
@@ -23,18 +23,19 @@ def selection_menu_options():
 
 def job_search():
     jobs = []
-    auth.file_job_read(jobs)
-    print("\n*****Job Search*****")
+    util.file_job_read(jobs)
+
     while True:
         print("\n****** JOB SEARCH ******")
         print("***(1) FIND JOB***")
         print("***(2) CREATE JOB***")
+        print("***(3) RETURN TO SELECTION MENU***")
         job_selection = input("Please select what option would would like to go to (press 1-2): ")
-        if job_selection in ['1','2']:
+        if job_selection in ['1','2', '3']:
             
             #Search for a Job
             if job_selection == '1':
-              print("Under Construction...\n")
+              print("\nUnder construction, check back later...")
               continue
                 
             #Create Job
@@ -47,16 +48,19 @@ def job_search():
                     salary = input("Enter the Job Salary: ")
                     job = {"title" : title, "description" : description, "employer" : employer, "location" : location, "salary" : salary}
                     jobs.append(job)
-                    auth.file_job_save(jobs)
+                    util.file_job_save(jobs)
                 else:
                     print("All permitted jobs have been created, please come back later")
+
+            elif job_selection == '3':
+                return
                 
         else:
             print("\nInvalid option. Please try again.\n")
             continue
 
         end_search = input("\nEnd your search? (Return to previous menu) (y/n): ")
-        while not valid.validate_input(end_search):
+        while not valid.validate_input_yn(end_search):
             print("\nInvalid option. Please try again.")
             end_search = input("\nEnd your search? (Return to previous menu) (y/n): ")
 
@@ -75,7 +79,7 @@ def skill_selection():
     while True:
         choice = input("Please select which skill you would like to develop/work on (press 1-5): ") 
         if choice in ['1', '2', '3', '4', '5']:
-            print("\nUnder construction, check back later.")
+            print("\nUnder construction, check back later...")
             return 
         elif choice == '6':
             return
