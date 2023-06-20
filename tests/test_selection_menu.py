829538acc7313dc4babf24393fbc2c0b64732f25
Ali_Asl <48028559+Aslanbayli@users.noreply.github.com>
2023-06-19 21:45:09 -0400

    pytest-selection

---
+++ tests/test_selection_menu.py
@@ -0,0 +1,77 @@
+from selection import selection_menu as select
+
+def test_selection_menu_options(monkeypatch, capsys):
+    inputs = iter(['2', '6'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    user = ["username", "first_name", "last_name", "language"]
+    select.selection_menu_options(user)
+    
+    out, _ = capsys.readouterr()
+    assert out == "\n****** SELECTION MENU ******\n\
+***(1) SEARCH FOR A JOB***\n\
+***(2) FIND SOMEONE YOU KNOW***\n\
+***(3) LEARN A NEW SKILL***\n\
+***(4) USEFUL LINKS***\n\
+***(5) IMPORTANT LINKS***\n\
+***(6) LOG OUT***\n\
+\nUnder construction, check back later....\n\
+\n****** SELECTION MENU ******\n\
+***(1) SEARCH FOR A JOB***\n\
+***(2) FIND SOMEONE YOU KNOW***\n\
+***(3) LEARN A NEW SKILL***\n\
+***(4) USEFUL LINKS***\n\
+***(5) IMPORTANT LINKS***\n\
+***(6) LOG OUT***\n"
+
+def test_job_search(monkeypatch, capsys):
+    inputs = iter(['1', '3', 'y'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    user = ["username", "first_name", "last_name", "language"]
+    select.job_search(user)
+
+    out, _ = capsys.readouterr()
+    assert out == "\n****** JOB SEARCH ******\n\
+***(1) FIND JOB***\n\
+***(2) CREATE JOB***\n\
+***(3) RETURN TO SELECTION MENU***\n\
+\nUnder construction, check back later...\n\
+\n****** JOB SEARCH ******\n\
+***(1) FIND JOB***\n\
+***(2) CREATE JOB***\n\
+***(3) RETURN TO SELECTION MENU***\n"
+
+def test_skill_selection(monkeypatch, capsys):
+    monkeypatch.setattr('builtins.input', lambda _: "1")
+    select.skill_selection()
+
+    out, _ = capsys.readouterr()
+    assert out == "\n****** SKILL SELECTION ******\n\
+***(1) TIME MANAGEMENT SKILLS***\n\
+***(2) CODING LANGUAGE SKILLS***\n\
+***(3) PUBLIC SPEAKING SKILLS***\n\
+***(4) PERSONAL FINANCE MANAGEMENT SKILLS***\n\
+***(5) COLLABORATION SKILLS***\n\
+*(6) RETURN TO PREVIOUS SCREEN*\n\
+\nUnder construction, check back later...\n"
+
+def test_useful_links(monkeypatch, capsys):
+    inputs = iter(['2', '5'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    res = select.useful_links("not_logged_in")
+
+    out, _ = capsys.readouterr()
+    assert out == "\n****** USEFUL LINKS ******\n\
+***(1) GENERAL***\n\
+***(2) BROWSE INCOLLEGE***\n\
+***(3) BUSINESS SOLUTIONS***\n\
+***(4) DIRECTORIES***\n\
+***(5) RETURN***\n\
+\nUnder construction, check back later ...\n\
+\n****** USEFUL LINKS ******\n\
+***(1) GENERAL***\n\
+***(2) BROWSE INCOLLEGE***\n\
+***(3) BUSINESS SOLUTIONS***\n\
+***(4) DIRECTORIES***\n\
+***(5) RETURN***\n"
+
+    assert res == "menu"
